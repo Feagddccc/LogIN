bfaf0ab93b66e4fd25b661a69693fd6a598ae7cf
AndyP <firreatwill@mail.ru>
2023-07-30 17:35:36 +0300

    connect code to board

--- LogIN/ViewController.swift
+++ LogIN/ViewController.swift
@@ -12,18 +12,45 @@ final class LoginViewController: UIViewController {
     @IBOutlet var userNameTextField: UITextField!
     @IBOutlet var passwordTextField: UITextField!
     
-    private let user = "User"
-    private let password = "Password"
+    private let user = "Admin"
+    private let password = "1234"
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         guard let welcomeVC = segue.destination as? WelcomeViewController else { return }
         welcomeVC.user = user
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    @IBAction func logInPressed() {
+        guard userNameTextField.text == user, passwordTextField.text == password else {
+            showAlert(
+                title: "Invalid login or password",
+                message: "Please, enter correct login and password",
+                textField: passwordTextField
+            )
+            return
+        }
+        performSegue(withIdentifier: "openWelcomeVC", sender: nil)
     }
-
+    
+    @IBAction func forgotRegisterData(_ sender: UIButton) {
+        sender.tag == 0
+            ? showAlert(title: "Attention!", message: "Your name is \(user)")
+            : showAlert(title: "Attention!", message: "Your password is \(password)")
+    }
+    
+    @IBAction func unwindSegue(segue: UIStoryboardSegue) {
+        userNameTextField.text = ""
+        passwordTextField.text = ""
+    }
+    
+    private func showAlert(title: String, message: String, textField: UITextField? = nil) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default) { _ in
+            textField?.text = ""
+        }
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
 
 }
 
